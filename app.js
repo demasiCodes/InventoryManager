b8b22727ad72db489953c3f1c1ea81e598cc2319
demasiCodes <112286074+demasiCodes@users.noreply.github.com>
2023-07-09 16:32:49 -0400

    Search Bar UI

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 window.addEventListener('DOMContentLoaded', function () {
-    var titleElement = document.getElementById('title');
-    var bodyElement = document.getElementById('body');
+    const searchInput = document.getElementById('search-input');
+    const searchIcon = document.getElementById('search-icon');
+    const searchButton = document.querySelector('.search-bar button');
 
-    // Update the title
-    titleElement.textContent = 'Inventory Manager';
-
-    // Update the body
-    bodyElement.textContent = 'Use Search Bar to Track Items:';
+    searchInput.addEventListener('input', function () {
+        const inputValue = searchInput.value;
+        if (inputValue !== '') {
+            searchIcon.style.fill = 'white'; // Change the color of the icon to white when there is input
+            searchButton.style.background = 'green'; // Change the background color to green when there is input
+        } else {
+            searchIcon.style.fill = 'black'; // Revert the color of the icon to the original color when input is empty
+            searchButton.style.background = 'rgb(154, 153, 153)'; // Revert the background color to the original color when input is empty
+        }
+    });
 });
