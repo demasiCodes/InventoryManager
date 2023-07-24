c67fa56d41f72d6de8dc999d9ef98e13fef9f3dd
demasiCodes <112286074+demasiCodes@users.noreply.github.com>
2023-07-24 12:37:48 -0400

    Implemented Pop-Ups

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ class Item {
     getName() {
         return this.name;
     }
-    getPrice() {
-        return this.price;
-    }
     getQuantity() {
         return this.quantity;
     }
@@ -48,39 +45,59 @@ window.addEventListener('DOMContentLoaded', function () {
     /******** Item List ********/
     const blockContainer = document.querySelector('.block-container');
     const addButton = document.getElementById('add-button');
+    const modal = document.getElementById('myModal'); // new item pop-ups
+    const confirmButton = document.getElementById('confirm'); // new item pop-ups
+    const cancelButton = document.getElementById('cancel'); // new item pop-ups
     let blocksData = [];
     // Load blocks from local storage
     if (localStorage.getItem('blocksData')) {
-        blocksData = JSON.parse(localStorage.getItem('blocksData')).map(itemData => new Item(itemData.name, itemData.quantiy, itemData.description));
+        blocksData = JSON.parse(localStorage.getItem('blocksData')).map(itemData => new Item(itemData.name, itemData.quantity, itemData.description));
         renderBlocks();
     }
     // Add blocks
     addButton.addEventListener('click', function () {
-        // Create a new block and item element
-        const newItem = new Item('New Item', 0, 'Default description');
-        const newBlock = document.createElement('div');
-        newBlock.classList.add('block');
-        newBlock.textContent = newItem.getName();
-        // Append the new block to the container
-        blockContainer.appendChild(newBlock);
-        // Store the block data in the array
-        blocksData.push(newItem);
-        saveBlocksData();
-        renderBlocks();
+        modal.style.display = 'block';
     });
-    // Remove blocks
-    blockContainer.addEventListener('click', function (event) {
-        // Check if the click target is a block element
-        if (event.target.classList.contains('block')) {
-            // Remove the clicked block
-            event.target.remove();
-            // Remove the block data from the array
-            const index = Array.from(blockContainer.children).indexOf(event.target);
-            blocksData.splice(index, 1);
+    confirmButton.addEventListener('click', function () {
+        // Prompt for user input
+        const name = document.getElementById('name').value;
+        const quantity = parseInt(document.getElementById('quantity').value);
+        const description = document.getElementById('description').value;
+        // Check if all fields are filled
+        if (name && !isNaN(quantity) && description) {
+            // Create a new block and item element
+            const newItem = new Item(name, quantity, description);
+            const newBlock = document.createElement('div');
+            newBlock.classList.add('block');
+            newBlock.textContent = newItem.getName();
+            // Append the new block to the container
+            blockContainer.appendChild(newBlock);
+            // Store the block data in the array
+            blocksData.push(newItem);
             saveBlocksData();
-            // Decrement item count
-            Item.decrementItemCount();
             renderBlocks();
+            // Close the modal after adding the item
+            clearInputFields();
+            modal.style.display = 'none';
+        } 
+        else {
+            alert('Please fill in all fields.');
+        }
+    });
+    function clearInputFields() {
+        document.getElementById('name').value = '';
+        document.getElementById('quantity').value = '';
+        document.getElementById('description').value = '';
+    }
+    cancelButton.addEventListener('click', function () {
+        // Close the modal without adding the item
+        clearInputFields();
+        modal.style.display = 'none';
+    });
+    // Close the modal when clicking anywhere outside of the modal
+    window.addEventListener('click', function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none'; 
         }
     });
     // Render storage
@@ -88,24 +105,65 @@ window.addEventListener('DOMContentLoaded', function () {
         // Clear the block container
         blockContainer.innerHTML = '';
         // Create and append blocks based on the data array
-        blocksData.forEach(function (item) {
+        blocksData.forEach(function (item, index) {
             const block = document.createElement('div');
             block.classList.add('block');
             // Create a paragraph element for displaying the item name and description
             const itemInfo = document.createElement('p');
-            itemInfo.textContent = item.getName() + ' \n ' + item.getDescription();
+            itemInfo.textContent = item.getName() + ' : ' + item.getDescription() + ' : ' + item.getQuantity();
             // append info
             block.appendChild(itemInfo);
             blockContainer.appendChild(block);
+            // Add an event listener to each block for options
+            block.addEventListener('click', function () {
+                showModalAttributes(index);
+            });
         });
         // Update the item count display
         const itemCountElement = document.getElementById('item-count');
         itemCountElement.textContent = 'Total Items: ' + Item.getItemCount();
     }
+    function showModalAttributes(index) {
+        const modal = document.getElementById('modalAttributes');
+        const nameInput = document.getElementById('nameInput');
+        const descriptionInput = document.getElementById('descriptionInput');
+        const quantityInput = document.getElementById('quantityInput');
+        // Show the modal
+        modal.style.display = 'block';
+        // Fill the input boxes with the current values
+        nameInput.value = blocksData[index].getName();
+        descriptionInput.value = blocksData[index].getDescription();
+        quantityInput.value = blocksData[index].getQuantity();
+        // Add event listener to the "Save Changes" button
+        document.getElementById('saveChangesButton').addEventListener('click', function () {
+            // Update the attributes with the new values
+            blocksData[index].setName(nameInput.value);
+            blocksData[index].setDescription(descriptionInput.value);
+            blocksData[index].setQuantity(parseInt(quantityInput.value));
+            saveBlocksData();
+            renderBlocks();
+            modal.style.display = 'none';
+        });
+        // Add event listener to the "Remove Item" button
+        document.getElementById('removeItemButton').addEventListener('click', function () {
+            // Remove the clicked block
+            block.remove();
+            // Remove the block data from the array
+            blocksData.splice(index, 1);
+            saveBlocksData();
+            // Decrement item count
+            Item.decrementItemCount();
+            modal.style.display = 'none';
+        });
+        // Add event listener to the "Cancel" button
+        document.getElementById('cancelButton').addEventListener('click', function () {
+            modal.style.display = 'none';
+        });
+    }
     // Save blocks
     function saveBlocksData() {
         // Convert Item objects to plain data objects for storage
-        const plainData = blocksData.map(item => ({ name: item.getName(), price: item.getPrice(), quantity: item.getQuantity() }));
+        const plainData = blocksData.map(item => ({ name: item.getName(), quantity: item.getQuantity(), description: item.getDescription() }));
         // Save the block data to local storage
         localStorage.setItem('blocksData', JSON.stringify(plainData));
     }
