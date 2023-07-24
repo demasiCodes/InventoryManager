927cfffa130eadd0917fea764380bc652ffb2c83
demasiCodes <112286074+demasiCodes@users.noreply.github.com>
2023-07-24 10:03:07 -0400

    Implemented Item Class & Spring

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+/******** Item Class ********/
+class Item {
+    constructor(name, quantity, description) {
+        this.name = name;
+        this.quantity = quantity;
+        this.description = description;
+        Item.itemCount++;
+    }
+    getName() {
+        return this.name;
+    }
+    getPrice() {
+        return this.price;
+    }
+    getQuantity() {
+        return this.quantity;
+    }
+    getDescription() {
+        return this.description;
+    }
+    static decrementItemCount() {
+        Item.itemCount--;
+    }
+    static getItemCount() {
+        return Item.itemCount;
+    }
+}
+Item.itemCount = 0;
+/******** End Item Class ********/
 window.addEventListener('DOMContentLoaded', function () {
     /******** Search Bar ********/
     const searchInput = document.getElementById('search-input');
@@ -15,26 +44,31 @@ window.addEventListener('DOMContentLoaded', function () {
         searchInput.value = '';
         searchBarContainer.classList.remove('active'); // Remove the .active class
     });
+    /******** End Search Bar ********/
     /******** Item List ********/
     const blockContainer = document.querySelector('.block-container');
     const addButton = document.getElementById('add-button');
     let blocksData = [];
     // Load blocks from local storage
     if (localStorage.getItem('blocksData')) {
-        blocksData = JSON.parse(localStorage.getItem('blocksData'));
+        blocksData = JSON.parse(localStorage.getItem('blocksData')).map(itemData => new Item(itemData.name, itemData.quantiy, itemData.description));
         renderBlocks();
     }
+    // Add blocks
     addButton.addEventListener('click', function () {
-        // Create a new block element
+        // Create a new block and item element
+        const newItem = new Item('New Item', 0, 'Default description');
         const newBlock = document.createElement('div');
         newBlock.classList.add('block');
-        newBlock.textContent = 'New Block';
+        newBlock.textContent = newItem.getName();
         // Append the new block to the container
         blockContainer.appendChild(newBlock);
         // Store the block data in the array
-        blocksData.push('New Block');
+        blocksData.push(newItem);
         saveBlocksData();
+        renderBlocks();
     });
+    // Remove blocks
     blockContainer.addEventListener('click', function (event) {
         // Check if the click target is a block element
         if (event.target.classList.contains('block')) {
@@ -44,23 +78,36 @@ window.addEventListener('DOMContentLoaded', function () {
             const index = Array.from(blockContainer.children).indexOf(event.target);
             blocksData.splice(index, 1);
             saveBlocksData();
-        } 
+            // Decrement item count
+            Item.decrementItemCount();
+            renderBlocks();
+        }
     });
     // Render storage
     function renderBlocks() {
         // Clear the block container
         blockContainer.innerHTML = '';
-
         // Create and append blocks based on the data array
-        blocksData.forEach(function (blockText) {
+        blocksData.forEach(function (item) {
             const block = document.createElement('div');
             block.classList.add('block');
-            block.textContent = blockText;
+            // Create a paragraph element for displaying the item name and description
+            const itemInfo = document.createElement('p');
+            itemInfo.textContent = item.getName() + ' \n ' + item.getDescription();
+            // append info
+            block.appendChild(itemInfo);
             blockContainer.appendChild(block);
         });
+        // Update the item count display
+        const itemCountElement = document.getElementById('item-count');
+        itemCountElement.textContent = 'Total Items: ' + Item.getItemCount();
     }
+    // Save blocks
     function saveBlocksData() {
+        // Convert Item objects to plain data objects for storage
+        const plainData = blocksData.map(item => ({ name: item.getName(), price: item.getPrice(), quantity: item.getQuantity() }));
         // Save the block data to local storage
-        localStorage.setItem('blocksData', JSON.stringify(blocksData));
+        localStorage.setItem('blocksData', JSON.stringify(plainData));
     }
 });
+/******** End Item List ********/
---
+++ inventoryapp/src/test/java/com/demasicodes/inventoryapp/InventoryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demasicodes.inventoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
